b393c3e0a6747bebf6ce1645b21e6fc696f45f41
Yuxuan Liu <umaumaliu@outlook.com>
2023-10-20 17:21:52 +0800

    update

--- basic_window.cpp
+++ basic_window.cpp
@@ -6,10 +6,10 @@ int* basicwindow(int* input)               //该程序主管图形界面
 {
 	std::cout << std::endl<<std::endl<< "---------------------START---------------------" << std::endl<<std::endl<<std::endl;
 
-	std::cout << "Level infprmation:   ";      //输出关卡信息
-	std::cout << "level: " << input[0];            //级别
-	std::cout << " Inbox block: " << input[1];     //积木数目
-	std::cout << " block information: ";           
+	std::cout << "Level infprmation:   "<<std::endl;      //输出关卡信息
+	std::cout << "level: " << input[0] <<std::endl;            //级别
+	std::cout << "Inbox block: " << input[1] << std::endl;     //积木数目
+	std::cout << "block information: ";           
 	for (int i = 2; i <= 21; ++i)
 	{
 		if (input[i] == 0)
@@ -19,9 +19,9 @@ int* basicwindow(int* input)               //该程序主管图形界面
 		std::cout << input[i] << " , ";           //积木信息
 	}
 
-	std::cout << "Enable place: " << input[22];   //可用空地数
+	std::cout << "Enable place: " << input[22]<<std::endl;   //可用空地数
 
-	std::cout << " Enable order: ";
+	std::cout << "Enable order: ";
 	if (input[23])
 		std::cout << "inbox , ";
 	if (input[24])
@@ -39,7 +39,7 @@ int* basicwindow(int* input)               //该程序主管图形界面
 	if (input[30])
 		std::cout << "jumpifzero , ";                     //可用指令集，用文件中的1,0来代表该指令是否可用
 
-	std::cout << "Output order: ";
+	std::cout <<std::endl<< "Output order: ";
 	for (int i = 31; i <= 41; ++i)
 	{
 		if (input[i] == 0)
@@ -52,10 +52,19 @@ int* basicwindow(int* input)               //该程序主管图形界面
 	std::cout << "\n\n\n\n";                          //图形界面正式开始
 
 	std::cout << "     +---+";
-	
+	int type = 7;
+
+	//for (int type = 7; type < 30; type += 7)
+	//{
+		for (int i = 0; i < type; ++i)
+			std::cout << " ";
+		std::cout << "+---+";
+		for (int i = 0; i < 30 - type; ++i)
+			std::cout << " ";
+	//}
+	for (int i = 0; i < 10; ++i)
+		std::cout << " ";
 
 
-
-
-
+	return 0;
 }
