a03f4dbf28a4a4725a77c72c7d8143a87f588303
rosist <2779477248@qq.co,[D[D[D[D[D[D[D[C[C[C[C[C[C[m>
2023-12-30 00:20:40 +0800

    增加了开始界面的动画

--- main-code.cpp
+++ main-code.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include"design.h"
 #include <fstream>
+#include <chrono>
+#include<thread>
+using namespace std;
 
 int main()
 {
+	cout << " __       __            __                                             \n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "|  \\  _  |  \\          |  \\                                            \n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "| $$ / \\ | $$  ______  | $$  _______   ______   ______ ____    ______  \n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "| $$/  $\\| $$ /      \\ | $$ /       \\ /      \\ |      \\    \\  /      \\ \n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "| $$  $$$\\ $$|  $$$$$$\\| $$|  $$$$$$$|  $$$$$$\\| $$$$$$\\$$$$\\|  $$$$$$\\\n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "| $$ $$\\$$\$$| $$    $$| $$| $$      | $$  | $$| $$ | $$ | $$| $$    $$\n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "| $$$$  \\$$$$| $$$$$$$$| $$| $$_____ | $$__/ $$| $$ | $$ | $$| $$$$$$$$\n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "| $$$    \\$$$ \\$$     \\| $$ \\$$     \\ \\$$    $$| $$ | $$ | $$ \$$     \\\n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+	cout << "   \\$$      \\$$  \\$$$$$$$ \\$$  \\$$$$$$$  \\$$$$$$  \\$$  \\$$  \\$$  \\$$$$$$$\n\n\n";
+	this_thread::sleep_for(chrono::milliseconds(700));
+
 	std::cout << "Welcome to the game.\n";
 	std::cout << "Please choose your level.\n";
 	
